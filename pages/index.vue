7026c041d44dd02289fd64068e2e9e4f46ad5afb
zaity74 <57995722+zaity74@users.noreply.github.com>
2021-06-15 18:54:07 +0200

    add netifly admin on my site

--- pages/index.vue
+++ pages/index.vue
@@ -241,6 +241,11 @@
 
 <script>
 export default {
+   head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
   data: () => {
     return {
     index: 0,
